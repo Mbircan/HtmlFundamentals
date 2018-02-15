6129ef3ed0c09336fd8b4d427825cb8882dca452
mbircan9 <mbircan9@hotmail.com>
2018-02-15 19:48:17 +0300

    Add project files.

---
+++ BootStrapFundamentals/Scripts/app.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <reference path="jquery-ui-1.12.1.js" />
+/// <reference path="bootstrap.js" />
+
+
+$("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [50, 100],
+    slide: function (event, ui) {
+        $("#amount").val("$" + ui.values[0] + "-$" + ui.values[1])
+    }
+});
---
+++ JavaScriptFundamentals/Scripts/kadastro.js
@@ -0,0 +1,73 @@
+﻿$(function () {
+
+    console.log("yüklendi");
+    $("#btnkonum").click(function () {
+        navigator.geolocation.getCurrentPosition(getposition);
+    });
+});
+function getposition(position) {
+    var konum = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var mapdiv =document.getElementById("map");
+    var settings = {
+        center: konum,
+        zoom: 15,
+        mapTypeId: 'terrain',
+        mapTypeControl:true,
+    navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+    }
+    var map = new google.maps.Map(mapdiv, settings)
+    var marker = new google.maps.Marker({
+        position: konum,
+        map: map,
+        title: 'Şu an buradasınız',
+        animation:google.maps.Animation.DROP
+    });
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
+
+    var torpak = new google.maps.LatLng(41.4332655, 32.0412304);
+    var service = new google.maps.DistanceMatrixService();
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsService.route({
+        origin: konum,
+        destination: torpak,
+        travelMode:google.maps.TravelMode.DRIVING
+    }, function (response, status) {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(response);
+        }
+        else{
+            alert("Rota çizilemedi. "+status);
+        }
+    });
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById("panel"));
+    service.getDistanceMatrix(
+      {
+          origins: [konum],
+          destinations: [torpak],
+          travelMode: 'DRIVING',
+          drivingOptions:{
+              departureTime:new Date(),
+              trafficModel:'bestguess'
+          },
+          unitSystem:google.maps.UnitSystem.METRIC,
+          avoidHighways: true,
+          avoidTolls: true,
+      }, distance);
+}
+function distance(response, status) {
+    console.log(response);
+    console.log(status);
+    if (status != 'OK'){
+        alert('Mesafe ölçülemedi');
+    }
+    else {
+        $("#bilgi").html("Gidilecek: " + response.destinationAddresses[0] + "<br/>Uzaklık: " + response.rows[0].elements[0].distance.text + "<br/>Süre: " + response.rows[0].elements[0].duration.text + "<br/>Trafik ile: " + response.rows[0].elements[0].duration_in_traffic.text);
+    }
+
+}
